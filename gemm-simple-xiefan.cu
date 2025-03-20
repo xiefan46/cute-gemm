a15a437b8d34069833d89c7b7251e125c88f4a62
Fan Xie <xiefan.fxie@bytedance.com>
2025-03-20 21:08:08 +0800

    modify

--- gemm-simple-xiefan.cu
+++ gemm-simple-xiefan.cu
@@ -39,7 +39,7 @@ int main() {
 
   using MMA = decltype(make_tiled_mma(mma_atom{}, make_layout(Shape<_2, _2, _1>{}), make_layout(Shape<_1, _2, _1>{})));
 
-  std::cout<<"MMA: "<<MMA{}<<" size mma: "<<size(MMA{})<<std::endl;
+  std::cout<<" size mma: "<<size(MMA{})<<std::endl;
 
 }
 
